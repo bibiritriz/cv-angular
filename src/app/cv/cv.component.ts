3ceb1f314f6fef0f6cb152d0bf092051909a584d
Bia <ntenhodoritosmasdepende@gmail.com>
2025-02-18 20:12:17 -0300

    :sparkles: feat(cv): criando estrutura completa de HTML para o componente

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './cv.component.css'
 })
 export class CvComponent {
-
+  nome_sobrenome = "Beatriz Camargo";
+  email = "beatrizcamargo@gmail.com";
+  telefone = "11 9156567243";
+  endereco = "Rua Não Sei Onde, Bairro Barroso, 873";
+  linkedin = "https://www.linkedin.com/";
 }
