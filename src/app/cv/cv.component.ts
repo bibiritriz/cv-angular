8f1b5735a01df48ee83ecf310a8dc677b6c16342
Bia <ntenhodoritosmasdepende@gmail.com>
2025-02-20 16:50:55 -0300

    :lipstick: feat: Estilização CSS da seção da educação e competências

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class CvComponent {
   nome_sobrenome = "Beatriz Camargo";
-  email = "beatrizcamargo@gmail.com";
+  email = "beacamargo@gmail.com";
   telefone = "(11) 99999-9999";
   endereco = "Rua dos girassóis 555, 41444-555 Curitiba";
   linkedin = "https://www.linkedin.com/in/beatrizcammargo/";
+  linkedin_text = "in/beatrizcammargo/"
   crea = "PR-143251/D"
 }
